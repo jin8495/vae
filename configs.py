42b9f62fe1888617425182d5c7996c36e5411c24
Hoyong Jin <jin8495@gmail.com>
2021-12-01 10:53:38 +0900

    A code for saving

--- configs.py
+++ configs.py
@@ -1,17 +1,20 @@
 # dataset related
-dataset_name = "COCO"
-resize_h, resize_w = 256, 320
+# dataset_name = "COCO"
+# resize_h, resize_w = 256, 320
+dataset_name = "MNIST"
+resize_h, resize_w = 28, 28
 
 # dataloader related
-batch_size = 128
+batch_size = 64
 num_workers = 12
 prefetch_factor = 4
 
 # model related
 input_ch = 1 if dataset_name == "MNIST" else 3
-channels = [input_ch, 32, 64, 128, 256, 512, 1024]
+# channels = [input_ch, 32, 64, 128, 256, 512, 1024]
+channels = [input_ch, 64, 128]
 num_z = 1024
-num_samples = 32
+num_samples = 12
 
 # training related
 epochs = 100
